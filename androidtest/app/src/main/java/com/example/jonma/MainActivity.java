7fe8071cb636a64acf53818280743069c7688fc5
Jon <420407115@qq.com>
2018-11-24 21:36:00 +0800

    程序实现Intent的传入和传回值

--- androidtest/app/src/main/java/com/example/jonma/MainActivity.java
+++ androidtest/app/src/main/java/com/example/jonma/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jonma;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,12 +10,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private  Button button;
     private  Button button1;
-
+    private TextView textView;
+    private TextView textView1;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menumain,menu);
@@ -42,26 +46,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button1 = (Button)findViewById(R.id.jion) ;
         button.setOnClickListener(this);
         button1.setOnClickListener(this);
+        textView = (TextView)findViewById(R.id.main);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case 1:
+                if (requestCode == 1){
+                    String returnData = data.getStringExtra("data_return");
+                    textView.setText(returnData);
+                    Log.d("MainActivity",returnData);
+                }
+                break;
+                default:
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.inser:
+                String date = "MainActivity import date";
                 Intent intent = new Intent(MainActivity.this,secondActivity.class);
+                intent.putExtra("tel",date);
                 startActivity(intent);
-                Log.i("add","单击完成");
-                Toast.makeText(this,"SD成功",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.jion:
-
-
+                Intent intent1 = new Intent(MainActivity.this,phoneActivity.class);
+                intent1.putExtra("data","首页传入的数据");
+                startActivityForResult(intent1,1);
                 break;
                 default:
                     break;
-
         }
     }
-
-
 }
--- androidtest/app/src/main/java/com/example/jonma/phoneActivity.java
+++ androidtest/app/src/main/java/com/example/jonma/phoneActivity.java
@@ -1,12 +1,42 @@
 package com.example.jonma;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class phoneActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       setContentView(R.layout.phone);
+        textView = (TextView)findViewById(R.id.thrid);
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        Log.d("phone","传入成功");
+        textView.setText(data);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return","传回首页数据成功");
+        setResult(1,intent);
+        finish();
+    }
+
+    public void onClick(View view) {
+
+        Intent intent1 = new Intent();
+        intent1.putExtra("data_return","传回首页数据成功");
+        setResult(RESULT_OK,intent1);
+        finish();
+        Log.i("艹你吗","回传成功");
+
     }
 }
